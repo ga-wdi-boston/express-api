854971ef4c6a50466f4ecd43ffe7b3b7229a7fc9
wmcb91 <wmcb91@gmail.com>
2016-10-12 14:48:51 -0400

    Annotate examples and finish books controller.

--- scripts/example-get.sh
+++ scripts/example-get.sh
@@ -1,1 +1,3 @@
 #!/bin/sh
+curl --include --request GET http://localhost:3000/examples/57fe58293c7b8dcd90b3ad53 \
+  # --header "Authorization: Token token=$TOKEN"
--- scripts/examples-create.sh
+++ scripts/examples-create.sh
@@ -1,1 +1,10 @@
 #!/bin/sh
+curl --include --request POST http://localhost:3000/examples \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=53P9RDK4xgaEQi4rkrTG+k2k64QVOO2djLErq9rP1x8=--cCRG7JQjYM4UWrvtVDqrsNhsZESEqmIXgMcNlzGE9Ec=" \
+  --data '{
+    "example": {
+      "text": "my first example text",
+      "_owner": "57fe57223c7b8dcd90b3ad51"
+    }
+  }'
