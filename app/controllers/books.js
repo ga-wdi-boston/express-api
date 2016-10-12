78321ea0e41acd61e035624fdaaf8b7dd431077b
james-coombs <coombsj4@yahoo.com>
2016-10-12 14:50:10 -0400

    Comment code for examples
    Comment code in app/controllers/examples.js.s

--- app/controllers/books.js
+++ app/controllers/books.js
@@ -1,1 +1,49 @@
 'use strict';
+
+const controller = require('lib/wiring/controller');
+const models = require('app/models');
+const Book = models.book;
+
+const authenticate = require('./concerns/authenticate');
+
+const index = (req, res, next) => {
+// request comes in wiht nothing
+  Book.find()
+  // is successful, creates response object with examples as json array
+  .then(books => res.json({ books }))
+  // if error, passes error through code to middleware to send error to client
+  .catch(error => next(error));
+};
+
+const show = (req, res, next) => {
+
+};
+
+const create = (req, res, next) => {
+  let book = Object.assign(req.body.book, {
+    _owner: req.currentUser._id,
+  });
+  Book.create(book)
+    .then(book => res.json({
+      book
+    }))
+    .catch(err => next(err));
+};
+
+const update = (req, res, next) => {
+
+};
+
+const destroy = (req, res, next) => {
+
+};
+
+module.exports = controller({
+  index,
+  show,
+  create,
+  update,
+  destroy,
+}, { before: [
+  { method: authenticate, except: ['index', 'show'] },
+], });
--- config/routes.js
+++ config/routes.js
@@ -8,6 +8,7 @@ module.exports = require('lib/wiring/routes')
 .root('root#root')
 
 // standards RESTful routes
+// Does not include New and Edit
 .resources('examples')
 
 // users of the app have special requirements
@@ -17,5 +18,11 @@ module.exports = require('lib/wiring/routes')
 .patch('/change-password/:id', 'users#changepw')
 .resources('users', { only: ['index', 'show'] })
 
+// books routes
+.post('/books', 'books#create')
+.get('/books', 'books#index')
+
 // all routes created
+
+
 ;
