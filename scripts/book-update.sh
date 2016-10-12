5cfdd32c28f299d3a274af170292e2d527456d53
rebekahheacock <rebekah.heacock@gmail.com>
2016-10-12 14:40:53 -0400

    Add curl scripts for books
    Index
    Show
    Create
    Update

--- scripts/book-update.sh
+++ scripts/book-update.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+curl --include --request PATCH http://localhost:3000/books/$ID \
+  --header "Authorization: Token token=$TOKEN" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "book": {
+      "title": "Even Better Title"
+    }
+  }'
--- scripts/books-create.sh
+++ scripts/books-create.sh
@@ -4,7 +4,7 @@ curl --include --request POST http://localhost:3000/books \
   --header "Content-Type: application/json" \
   --data '{
     "book": {
-      "title": "Example title",
-      "author": "Example author"
+      "title": "Better title",
+      "author": "Better author"
     }
   }'
