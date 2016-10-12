1a715566aa44417f4ebc4faef44e4d8251310a24
rebekahheacock <rebekah.heacock@gmail.com>
2016-10-12 14:37:26 -0400

    Rename examples-create.sh to example-create.sh

--- scripts/examples-create.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-curl --include --request POST http://localhost:3000/examples \
-  --header "Authorization: Token token=$TOKEN" \
-  --header "Content-Type: application/json" \
-  --data '{
-    "example": {
-      "text": "My first example text"
-    }
-  }'
