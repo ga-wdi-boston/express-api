6dd537b14d2abd74ba59b8b3564533e7c6fc1c19
kopiusmaximus <alexander.kopynec@gmail.com>
2016-10-12 14:34:24 -0400

    Complete training
    Complete training

--- scripts/examples-create.sh
+++ scripts/examples-create.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+curl --include --request POST http://localhost:3000/examples \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=qz6UUkR9zyJ8wN2fOqzHuXV9r5IYxxbECGfvGzjfgaY=--WVzpF+xeM4hCw/7PmCGb6NeSmK0t9qeIwq48wJExLZc=" \
+  --data '{
+    "example": {
+      "text": "my first example text"
+    }
+  }'
