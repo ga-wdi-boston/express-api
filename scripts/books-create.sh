02e83f75e56eabf9622b7ca2760bc08cccc8f5bc
nlkassad <nlkassad@hotmail.com>
2016-10-12 14:27:36 -0400

    Add Books Handlers with Examples Annotation
    Add handlers for books actions, also adds explainer annotation to
    examples controllers.

--- scripts/books-create.sh
+++ scripts/books-create.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+
+curl --include --request POST http://localhost:3000/books \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN" \
+  --data '{
+    "book": {
+      "title": "My First Book",
+      "author": "Alex K",
+    }
+  }'
--- scripts/example-get.sh
+++ scripts/example-get.sh
@@ -1,1 +1,3 @@
 #!/bin/sh
+
+curl --include --request GET http://localhost:3000/examples/57fe5815f5f6742d352c1691 \
